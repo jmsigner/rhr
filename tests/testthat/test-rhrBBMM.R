b30f8419113923dbc3e084213c2a055e323ffa8c
jmsigner <jmsigner@gmail.com>
2014-12-11 11:00:33 +0100

    more testing

---
+++ tests/testthat/test-rhrBBMM.R
@@ -0,0 +1,17 @@
+library(testthat)
+library(rhr)
+library(lubridate)
+data(datSH)
+
+context("rhrBBMM: test cases")
+datSH <- datSH[!duplicated(datSH[, 2:3]), ]
+dat1 <- datSH[, 2:3]
+dat1$ts <- ymd(datSH$day) + hms(datSH$time)
+
+test_that("BBMM works", {
+  ests <- rhrBBMM(dat1[, 1:2], dat1$ts)
+})
+
+
+
+
