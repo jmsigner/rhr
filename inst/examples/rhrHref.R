df4d5e9c747dd5597f4b8793479077d8fdb1f534
jmsigner <jmsigner@gmail.com>
2015-01-21 18:04:33 +0100

    examples

---
+++ inst/examples/rhrHref.R
@@ -0,0 +1,12 @@
+library(rhr)
+data(datSH)
+
+rhrHref(datSH[, 2:3])
+rhrHref(datSH[, 2:3], rescale = "unitvar")
+rhrHref(datSH[, 2:3], rescale = "xvar")
+
+kde1 <- rhrKDE(datSH[, 2:3], h = rhrHref(datSH[, 2:3])$h)
+kde2 <- rhrKDE(datSH[, 2:3], h = rhrHref(datSH[, 2:3], "unitvar")$h)
+kde3 <- rhrKDE(datSH[, 2:3], h = rhrHref(datSH[, 2:3], "xvar")$h)
+
+lapply(list(kde1, kde2, kde3), rhrArea)
